35711a3e301d8f2225825749152124869e18b18b
danwook <k.danwoo91@ibksystem.co.kr>
2021-07-16 16:15:16 +0900

    초기 설정

--- src/main/java/com/kdanwoo/jpademo/JpademoApplication.java
+++ src/main/java/com/kdanwoo/jpademo/JpademoApplication.java
@@ -1,9 +1,11 @@
 package com.kdanwoo.jpademo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class JpademoApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/kdanwoo/jpademo/JpademoApplicationTests.java
+++ src/test/java/com/kdanwoo/jpademo/JpademoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.kdanwoo.jpademo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @SpringBootTest
+@Slf4j
 class JpademoApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
+
     @Test
-    void contextLoads() {
+    void contextLoads(){
     }
 
 }
